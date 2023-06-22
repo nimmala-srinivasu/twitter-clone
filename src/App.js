0b5c8890199dfe417e97640df23eb9085abc28b8
nimmala-srinivasu <srinivasu4321@gmail.com>
2023-06-22 14:32:25 +0530

    widgets

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Feed from "./components/Feed";
+import Widgets from "./components/Widgets";
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
       <Feed />
+      <Widgets />
     </div>
   );
 }
