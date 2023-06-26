aa8f57f33fe6e5b4bd400a514b9be85fd1e3d008
nimmala-srinivasu <srinivasu4321@gmail.com>
2023-06-26 15:22:59 +0530

    post data to database

--- src/components/TweetBox.jsx
+++ src/components/TweetBox.jsx
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TweetBox.css";
 import { Avatar, Button } from "@mui/material";
+import db from "../firebase";
+import { addDoc, collection } from "firebase/firestore";
 
 function TweetBox() {
+  const [tweetMessage, setTweetMessage] = useState("");
+  // const [tweetImage, setTweeetImage] = setState('')
+
+  const sendTweet = async (e) => {
+    e.preventDefault();
+    try {
+      await addDoc(collection(db, "posts"), {
+        displayName: "NPS",
+        userName: "nps",
+        verified: false,
+        text: tweetMessage,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    setTweetMessage("");
+  };
+
   return (
     <div className="tweetBox">
       <form>
         <div className="tweetBox__input">
           <Avatar src="https://i.pinimg.com/564x/0c/f8/2c/0cf82cc12c00d79e8a5c08f953585ef7.jpg" />
-          <input placeholder="What's happening" type="text" />
+          <input
+            onChange={(e) => setTweetMessage(e.target.value)}
+            value={tweetMessage}
+            placeholder="What's happening"
+            type="text"
+          />
         </div>
-        <Button className="tweetBox__tweetButton">Tweet</Button>
+        <Button
+          onClick={sendTweet}
+          type="submit"
+          className="tweetBox__tweetButton"
+        >
+          Tweet
+        </Button>
       </form>
     </div>
   );
